fb63bd1d3390ce400c60673ffac95d51a07164b9
chronit <pv2066pv@gmail.com>
2026-09-03 21:08:39 +0900

    glass-redesign: 라이트 글래스로 전환 (오프화이트 + 소프트 브랜드 블롭 + 라이트 글래스 카드), 다크/곡선 제거

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ import UserProfile from './pages/UserProfile'
 import DmAutomation from './pages/DmAutomation'
 import PaymentResult from './pages/PaymentResult'
 import ErrorBoundary from './components/ErrorBoundary'
-import CurveBackground from './components/CurveBackground'
+import BlobBackground from './components/BlobBackground'
 import ErrorReportModal from './components/ErrorReportModal'
 import AdminFab from './components/AdminFab'
 import { installGlobalErrorCapture } from './lib/errorReport'
@@ -90,7 +90,7 @@ const App = () => {
   return (
   <BrowserRouter>
     <AnalysisProvider>
-    <CurveBackground />
+    <BlobBackground />
     <ScrollToTop />
     <ErrorReportModal />
     <AdminFab />
