4f9afd0f7c1f8fe53ddb25a7cf3f1f876605e95a
chronit <pv2066pv@gmail.com>
2026-09-03 21:34:08 +0900

    가독성: input 또렷한 흰필드로 + 카드 불투명도↑ + 블롭 톤다운(파란버튼 대비 회복)

--- src/components/BlobBackground.jsx
+++ src/components/BlobBackground.jsx
@@ -2,10 +2,10 @@ export default function BlobBackground() {
   const blob = { position: 'absolute', borderRadius: '50%', filter: 'blur(72px)' }
   return (
     <div aria-hidden="true" style={{ position: 'fixed', inset: 0, zIndex: 0, overflow: 'hidden', pointerEvents: 'none' }}>
-      <div style={{ ...blob, width: '46vw', height: '46vw', left: '-8vw', top: '-6vw', background: '#0064ff', opacity: .5 }} />
-      <div style={{ ...blob, width: '40vw', height: '40vw', right: '-6vw', top: '6vh', background: '#7c5cff', opacity: .5 }} />
-      <div style={{ ...blob, width: '42vw', height: '42vw', left: '20vw', bottom: '-14vw', background: '#22d3ee', opacity: .4 }} />
-      <div style={{ ...blob, width: '26vw', height: '26vw', right: '14vw', bottom: '8vh', background: '#ff7ab8', opacity: .28 }} />
+      <div style={{ ...blob, width: '46vw', height: '46vw', left: '-8vw', top: '-6vw', background: '#0064ff', opacity: .34 }} />
+      <div style={{ ...blob, width: '40vw', height: '40vw', right: '-6vw', top: '6vh', background: '#7c5cff', opacity: .34 }} />
+      <div style={{ ...blob, width: '42vw', height: '42vw', left: '20vw', bottom: '-14vw', background: '#22d3ee', opacity: .28 }} />
+      <div style={{ ...blob, width: '26vw', height: '26vw', right: '14vw', bottom: '8vh', background: '#ff7ab8', opacity: .2 }} />
     </div>
   )
 }
